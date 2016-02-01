521b20f0a3a58d89ec4aa05d4cc70100614523ee
Kevin Kirsche <kev.kirsche@gmail.com>
2016-02-01 09:51:25 -0500

    Add export policy support
    Signed-off-by: Kevin Kirsche <kev.kirsche@gmail.com>

--- apiPolicies.go
+++ apiPolicies.go
@@ -6,6 +6,32 @@ import (
 	"net/http"
 )
 
+// ExportPolicy exports the given policy in nessus (XML) format.
+// It requires an http.Client pointer to make the request to Nessus.
+func (c *Client) ExportPolicy(httpClient *http.Client, policyID int) (string, error) {
+	c.debugln("ListPolicy(): Building list policies URL")
+	url := fmt.Sprintf("https://%s:%s/policies/%d/export", c.ip, c.port, policyID)
+
+	statusCode, body, err := c.get(httpClient, url)
+	if err != nil {
+		return "", err
+	}
+
+	switch statusCode {
+	case 200:
+		c.debugln("ListPolicy(): Successfully retrieved policy list.")
+		return string(body), nil
+	default:
+		var err ErrorResponse
+		unmarshalError := json.Unmarshal(body, &err)
+		if unmarshalError != nil {
+			return "", unmarshalError
+		}
+		c.debugln("ListPolicy(): Policy list could not be retrieved.")
+		return "", fmt.Errorf("%s", err.Error)
+	}
+}
+
 // ListPolicy returns the policy list.
 // It requires an http.Client pointer to make the request to Nessus.
 func (c *Client) ListPolicy(httpClient *http.Client) (ListPolicyResponse, error) {
--- apiPolicies_test.go
+++ apiPolicies_test.go
@@ -9,7 +9,42 @@ import (
 	"testing"
 )
 
-func TestListPolicies(t *testing.T) {
+func TestExportPolicy(t *testing.T) {
+	testServer := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := `{"policies":"test"}` // Using JSON for CreateSession, otherwise this would return XML for ExportPolicy
+		fmt.Fprintln(w, response)
+	}))
+	defer testServer.Close()
+
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	httpClient := &http.Client{Transport: transport}
+
+	port := strings.Split(testServer.URL, ":")[2]
+
+	client := &Client{
+		username: "testU",
+		password: "testP",
+		ip:       "127.0.0.1",
+		port:     port,
+	}
+
+	client, err := client.CreateSession(httpClient)
+	if err != nil {
+		t.FailNow()
+	}
+
+	policies, err := client.ExportPolicy(httpClient, 25)
+	if err != nil || policies == "" {
+		fmt.Println(err)
+		t.FailNow()
+	}
+}
+
+func TestListPolicy(t *testing.T) {
 	testServer := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		response := `{"policies":[{"no_target":"false","template_uuid":"exampleuuid", "description":"This is an example Policy for GoNessus","name":"Example Policy","owner":"kkirsche","visibility":"shared","shared":1,"user_permissions":32,"last_modification_date":1454334708,"creation_date":1454334708,"owner_id":2,"id":25}]}`
