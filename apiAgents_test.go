7fbbc0f40db8131b1df975fcf26e19359a7a1879
Kevin Kirsche <kev.kirsche@gmail.com>
2016-02-01 18:13:31 -0500

    Added Nessus.agents to the tool
    Signed-off-by: Kevin Kirsche <kev.kirsche@gmail.com>

---
+++ apiAgents_test.go
@@ -0,0 +1,78 @@
+package nessus
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestDeleteAgent(t *testing.T) {
+	testServer := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := `{"token":"Example"}` // Return JSON for CreateSession's sake. This method doesn't return anything
+		fmt.Fprintln(w, response)
+	}))
+	defer testServer.Close()
+
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	httpClient := &http.Client{Transport: transport}
+
+	port := strings.Split(testServer.URL, ":")[2]
+
+	client := &Client{
+		username: "testU",
+		password: "testP",
+		ip:       "127.0.0.1",
+		port:     port,
+	}
+
+	client, err := client.CreateSession(httpClient)
+	if err != nil {
+		t.FailNow()
+	}
+
+	successfullyDeleted, err := client.DeleteAgent(httpClient, 1, 1)
+	if err != nil || successfullyDeleted != true {
+		t.FailNow()
+	}
+}
+
+func TestListAgents(t *testing.T) {
+	testServer := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := `{"agents":[{"distro": "Ubuntu","id": 1,"ip": "127.0.0.1","last_scanned": "Unknown format","name": "Agent 1","platform": "Linux","token": "Example Token","uuid": "Example UUID"}]}`
+		fmt.Fprintln(w, response)
+	}))
+	defer testServer.Close()
+
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	httpClient := &http.Client{Transport: transport}
+
+	port := strings.Split(testServer.URL, ":")[2]
+
+	client := &Client{
+		username: "testU",
+		password: "testP",
+		ip:       "127.0.0.1",
+		port:     port,
+	}
+
+	client, err := client.CreateSession(httpClient)
+	if err != nil {
+		t.FailNow()
+	}
+
+	agentsList, err := client.ListAgents(httpClient, 1)
+	if err != nil || agentsList.Agents[0].Distro != "Ubuntu" {
+		t.FailNow()
+	}
+}
