59d0f85f2fafe2bc0c6d582639c2bfb70bd14887
Kevin Kirsche <kev.kirsche@gmail.com>
2016-02-02 11:46:36 -0500

    Add create scan pipeline object
    Signed-off-by: Kevin Kirsche <kev.kirsche@gmail.com>

--- creator/creatorScan.go
+++ creator/creatorScan.go
@@ -1,20 +1,60 @@
 package nessusCreator
 
 import (
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"github.com/kkirsche/nessusControl/api"
+	"net/http"
 	"strings"
 	"sync"
 )
 
+func (c *Creator) createScan(createScanCh chan nessusAPI.CreateScan, skipTLSVerify bool) chan nessusAPI.CreateScanResponse {
+	c.debugln("createScan(): Creating scans from JSON")
+	createScanResponseCh := make(chan nessusAPI.CreateScanResponse)
+	wg := new(sync.WaitGroup)
+
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: skipTLSVerify},
+	}
+	httpClient := &http.Client{Transport: transport}
+
+	for createScanJSON := range createScanCh {
+		wg.Add(1)
+		go func(createScanJSON nessusAPI.CreateScan, wg *sync.WaitGroup, httpClient *http.Client) {
+			marshalledJSON, err := json.Marshal(createScanJSON)
+			if err != nil {
+				wg.Done()
+				return
+			}
+
+			createdScan, err := c.apiClient.CreateScan(httpClient, string(marshalledJSON))
+			if err == nil {
+				createScanResponseCh <- createdScan
+			}
+			wg.Done()
+		}(createScanJSON, wg, httpClient)
+	}
+
+	go func(wg *sync.WaitGroup, createScanResponseCh chan nessusAPI.CreateScanResponse) {
+		wg.Wait()
+		close(createScanResponseCh)
+	}(wg, createScanResponseCh)
+
+	return createScanResponseCh
+}
+
 // buildCreateScanJSON is used to create a struct which can be marshalled into
 // JSON and sent to a remote Nessus server.
 func (c *Creator) buildCreateScanJSON(requestScanch chan RequestedScan) chan nessusAPI.CreateScan {
+	c.debugln("buildCreateScanJSON(): Building JSON for Create Scan")
 	createScanJSONch := make(chan nessusAPI.CreateScan)
 	wg := new(sync.WaitGroup)
 
 	for requestedScan := range requestScanch {
 		wg.Add(1)
+		c.debugln("buildCreateScanJSON(): Building JSON for request ID #" + requestedScan.RequestID)
 		go func(requestedScan RequestedScan, wg *sync.WaitGroup, createScanJSONch chan nessusAPI.CreateScan) {
 			scanNameAndDescription := fmt.Sprintf("Scan Request #%s, Method: %s", requestedScan.RequestID, requestedScan.Method)
 			targets := strings.Join(requestedScan.TargetIPs, " ")
@@ -50,6 +90,7 @@ func (c *Creator) buildCreateScanJSON(requestScanch chan RequestedScan) chan nes
 // scanMethodToPolicyID takes a method which was extracted from
 // processRequestedScanFile() and returns the corresponding Nessus Policy ID.
 func (c *Creator) scanMethodToPolicyID(method string) string {
+	c.debugln("scanMethodToPolicyID(): Determining policy ID from scan method")
 	switch method {
 	case "allportswithping":
 		return "52"
--- creator/creatorScan_test.go
+++ creator/creatorScan_test.go
@@ -1,8 +1,68 @@
 package nessusCreator
 
 import (
+	"crypto/tls"
+	"fmt"
+	"github.com/kkirsche/nessusControl/api"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
+
+func TestCreateScanJSON(t *testing.T) {
+	testServer := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := `{"scan":{"container_id":0,"uuid":"template-9f6a69d0-709e-22b1-f696-e1de009f6a8faf787b8b14d3a111","name":"Example Scan","description":null,"policy_id":40,"scanner_id":1,"emails":null,"sms":null,"enabled":true,"use_dashboard":false,"dashboard_file":null,"scan_time_window":null,"custom_targets":"localhost","starttime":null,"rrules":null,"timezone":null,"notification_filters":null,"shared":0,"user_permissions":128,"default_permisssions":0,"owner":"testU","owner_id":3,"last_modification_date":1454379756,"creation_date":1454379756,"type":"public","id":41}}`
+		fmt.Fprintln(w, response)
+	}))
+	defer testServer.Close()
+
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	httpClient := &http.Client{Transport: transport}
+
+	port := strings.Split(testServer.URL, ":")[2]
+
+	client := nessusAPI.NewUsernameClient("127.0.0.1", port, "testU", "testP", false)
+
+	client, err := client.CreateSession(httpClient)
+	if err != nil {
+		t.FailNow()
+	}
+
+	creator := NewCreator("/some/place", client, false)
+	createScanCh := make(chan nessusAPI.CreateScan)
+
+	go func(createScanCh chan nessusAPI.CreateScan) {
+		for i := 0; i < 5; i++ {
+			createScanCh <- nessusAPI.CreateScan{
+				UUID: "TestUUID",
+				Settings: nessusAPI.CreateScanSettings{
+					Name:        "Request 1",
+					Description: "Request 2",
+					FolderID:    "1",
+					ScannerID:   "2",
+					PolicyID:    "3",
+					TextTargets: "123.12.45.0/24",
+					Launch:      "ONETIME",
+					Enabled:     false,
+					LaunchNow:   false,
+				},
+			}
+		}
+		close(createScanCh)
+	}(createScanCh)
+
+	createScanResponseCh := creator.createScan(createScanCh, true)
+	for createdScanResponse := range createScanResponseCh {
+		if createdScanResponse.Scan.UUID != "template-9f6a69d0-709e-22b1-f696-e1de009f6a8faf787b8b14d3a111" {
+			t.FailNow()
+		}
+	}
+}
 
 func TestBuildCreateScanJSON(t *testing.T) {
 	creator := Creator{}
