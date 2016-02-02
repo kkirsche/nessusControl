23f5fc149e9a3c1737ae05b612f12d5bdbd3e849
Kevin Kirsche <kev.kirsche@gmail.com>
2016-02-02 16:37:29 -0500

    Starting point for the test
    Signed-off-by: Kevin Kirsche <kev.kirsche@gmail.com>

---
+++ creator/creatorIngestPipeline_test.go
@@ -0,0 +1,30 @@
+package nessusCreator
+
+import (
+	"crypto/tls"
+	"github.com/kkirsche/nessusControl/api"
+	"net/http"
+	"os"
+	"testing"
+)
+
+// IngestPipeline is used to run the creator's ingest pipeline
+func TestIngestPipeline(t *testing.T) {
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	httpClient := &http.Client{Transport: transport}
+	debug := false
+	apiClient := nessusAPI.NewAccessTokenClient("127.0.0.1", "8834", "testU", "testP", debug)
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.FailNow()
+	}
+
+	creator := NewCreator(pwd+"/test/fixtures", apiClient, httpClient, debug)
+	err = creator.IngestPipeline(true)
+	if err != nil {
+		t.FailNow()
+	}
+}
