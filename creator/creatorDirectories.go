488a28eb8be0f614145c7312e2a87d5901341452
Kevin Kirsche <kev.kirsche@gmail.com>
2016-02-02 16:39:59 -0500

    Separate this function into it's own file
    Signed-off-by: Kevin Kirsche <kev.kirsche@gmail.com>

--- creator/creatorDirectories.go
+++ creator/creatorDirectories.go
@@ -1,26 +1,8 @@
 package nessusCreator
 
 import (
-	"io"
 	"os"
 )
-
-func isDirEmpty(name string) (bool, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return false, err
-	}
-	defer f.Close()
-
-	// read in ONLY one file
-	_, err = f.Readdir(1)
-
-	// and if the file is EOF... well, the dir is empty.
-	if err == io.EOF {
-		return true, nil
-	}
-	return false, err
-}
 
 func (c *Creator) removeTempDirIfEmpty() {
 	isEmpty, err := isDirEmpty(c.fileLocations.temporaryDirectory)
