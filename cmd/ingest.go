2f5339b9217a61b4cc6a2b36e26cfaf95fa2d641
Kevin Kirsche <kev.kirsche@gmail.com>
2016-02-05 09:06:08 -0500

    Begin adding cobra command
    Signed-off-by: Kevin Kirsche <kev.kirsche@gmail.com>

---
+++ cmd/ingest.go
@@ -0,0 +1,52 @@
+// Copyright © 2016 Kevin Kirsche <kev.kirsche@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// ingestCmd represents the ingest command
+var ingestCmd = &cobra.Command{
+	Use:   "ingest",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		fmt.Println("ingest called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(ingestCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// ingestCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// ingestCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
