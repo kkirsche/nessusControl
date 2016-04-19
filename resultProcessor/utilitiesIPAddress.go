0a3319063b8572a7aa786897cd9c8ca9893aab45
Kevin Kirsche <kev.kirsche@gmail.com>
2016-04-19 16:28:24 -0400

    Add inet_aton and inet6_aton functions in golang
    Signed-off-by: Kevin Kirsche <kev.kirsche@gmail.com>

--- resultProcessor/utilitiesIPAddress.go
+++ resultProcessor/utilitiesIPAddress.go
@@ -1,6 +1,7 @@
 package nessusProcessor
 
 import (
+	"encoding/hex"
 	"math/big"
 	"net"
 )
@@ -10,4 +11,22 @@ func IPv4ToInt(ip net.IP) int64 {
 	ipv4Int := big.NewInt(0)
 	ipv4Int.SetBytes(ip.To4())
 	return ipv4Int.Int64()
+}
+
+// IPv4ToHex converts an IPv4 net.IP object to a hexadecimal representaiton.
+// This function is the equivalent of inet6_aton({{ ipv4 address }}) in MySQL.
+func IPv4ToHex(ip net.IP) string {
+	ipv4Int := big.NewInt(0)
+	ipv4Int.SetBytes(ip.To4())
+	ipHex := hex.EncodeToString(ipv4Int.Bytes())
+	return ipHex
+}
+
+// IPv6ToHex converts an IPv6 net.IP object to a hexadecimal representaiton.
+// This function is the equivalent of inet6_aton({{ ipv6 address }}) in MySQL.
+func IPv6ToHex(ip net.IP) (string, error) {
+	ipv6Int := big.NewInt(0)
+	ipv6Int.SetBytes(ip.To16())
+	ipHex := hex.EncodeToString(ipv6Int.Bytes())
+	return ipHex, nil
 }
--- resultProcessor/utilitiesIPAddress_test.go
+++ resultProcessor/utilitiesIPAddress_test.go
@@ -16,3 +16,28 @@ func TestIPv4ToInt(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestIPv4ToHex(t *testing.T) {
+	ip := net.ParseIP("10.0.5.9")
+	if ip == nil {
+		t.FailNow()
+	}
+
+	ipHex := IPv4ToHex(ip)
+	if ipHex != "0a000509" {
+		t.FailNow()
+	}
+}
+
+func TestIPv6ToHex(t *testing.T) {
+	ip := net.ParseIP("fdfe::5a55:caff:fefa:9089")
+
+	ipHex, err := IPv6ToHex(ip)
+	if err != nil {
+		t.FailNow()
+	}
+
+	if ipHex != "fdfe0000000000005a55cafffefa9089" {
+		t.FailNow()
+	}
+}
