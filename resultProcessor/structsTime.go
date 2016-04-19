7d3f2f085065d5dc88f46dcd4b77fea70c0d74e2
Kevin Kirsche <kev.kirsche@gmail.com>
2016-04-19 10:02:47 -0400

    Add time structure to result processing and note link to creator
    Signed-off-by: Kevin Kirsche <kev.kirsche@gmail.com>

--- resultProcessor/structsTime.go
+++ resultProcessor/structsTime.go
@@ -2,6 +2,6 @@ package nessusCreator
 
 const (
 	// RFC3339Safe is RFC3339 with :'s replaced with dashes for filesystem safety
-	// This is also in nessusProcessor
+	// This is also in nessusCreator
 	RFC3339Safe = "2006-01-02T15-04-05Z07-00"
 )
